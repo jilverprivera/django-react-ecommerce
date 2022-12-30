5d75b9a17c1c6e62c536e2c7b003d9c74110efce
Jilver Pacheco <jilverrivera1@gmail.com>
2022-12-29 22:43:04 -0500

    adding cart and wishlist apps

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -3,20 +3,23 @@ from django.conf import settings
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
-
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
+
+from account.views import MyTokenObtainPairView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+
     path('api/', include('apps.category.api.urls')),
     path('api/', include('apps.product.api.urls')),
+    path('api/', include('apps.cart.api.urls')),
+    path('api/', include('apps.wishlist.api.urls')),
+
     path('', TemplateView.as_view(template_name='index.html')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
